f5936dd4c4eec1cc329b1cd5d18b8f93a489f10b
Jonathan Jiang <theNerdyMango@gmail.com>
2022-11-05 19:56:41 -0500

    Changes to Bag: initialized bag with correct amount of tiles

--- src/Game/Backend/Bag.java
+++ src/Game/Backend/Bag.java
@@ -1,28 +1,35 @@
 package Game.Backend;
-import java.lang.Math.*;
+import java.util.*;
+
 public class Bag{
-  private List<Tile> tiles;
-  private List<Tile> returnedTiles;
+    private List<Tile> tiles;
+    private List<Tile> returnedTiles;
+
+    public Bag(){
+        tiles = new ArrayList<Tile>();
+        returnedTiles = new ArrayList<Tile>();
+
+        for(var color : new Tile.TileColor[]{
+            Tile.TileColor.RED, Tile.TileColor.YELLOW, Tile.TileColor.BLACK, Tile.TileColor.BLUE, Tile.TileColor.WHITE
+        }){
+            for(int i = 0; i < 20; i++) {
+                returnedTiles.add(new Tile(color));
+            }
+        }
+
+        refillBack();
+    }
+
+    public Tile popTile(){
+        return tiles.remove(tiles.size() - 1);
+    }
+
+    public void returnTile(Tile t){
+        returnedTiles.add(t);
+    }
   
-  public Bag(){
-    tiles = new ArrayList<Tile>();
-    returnedTiles = new ArrayList<Tile>();
-  }
-  
-  public Tile getTile(){
-    Tile t = tiles.remove(Math.random() * tiles.size());
-    return t;
-  }
-  public void returnTile(Tile t){
-    returnedTiles.add(t); 
-  }
-  
-  public void refillBack(){
-    tiles.addAll(returnedTiles); 
-  }
-  
-  
-  
-  
-  
+    public void refillBack(){
+        tiles.addAll(returnedTiles);
+        Collections.shuffle(tiles);
+    }
 }

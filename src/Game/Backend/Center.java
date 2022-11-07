7d484a068dd2212007ef008e22b0bd96f2f19770
Jonathan Jiang <theNerdyMango@gmail.com>
2022-11-06 19:43:45 -0600

    Added some methods to Center

--- src/Game/Backend/Center.java
+++ src/Game/Backend/Center.java
@@ -2,9 +2,7 @@ package Game.Backend;
 
 import Engine.Core.GameObject;
 
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Center extends AbstractTileSet{
@@ -13,14 +11,51 @@ public class Center extends AbstractTileSet{
 
     public Center(){
         super();
-        firstPlayerTile =  new Tile();
+        firstPlayerTile =  new Tile(Tile.TileColor.FIRST_PLAYER);
         gameObject = createGameObject();
     }
 
+    @Override
+    protected GameObject createGameObject() {
+        return null;
+    }
+
     public boolean hasFirstPlayerTile(){
-        if(this.getTilesOfColor(Tile.TileColor.FIRST_PLAYER) != null){
-            return true;
+        return firstPlayerTile != null;
+    }
+
+    @Override
+    public List<Tile> getTilesOfColor(Tile.TileColor color) {
+        List<Tile> res = new ArrayList<>(super.getTilesOfColor(color));
+        if(firstPlayerTile != null) res.add(firstPlayerTile);
+        return res;
+    }
+
+    @Override
+    public List<Tile> removeTilesOfColor(Tile.TileColor color) {
+        List<Tile> res = new ArrayList<>(super.removeTilesOfColor(color));
+        if(firstPlayerTile != null){
+            res.add(firstPlayerTile);
+            firstPlayerTile = null;
         }
+        return res;
+    }
+
+    @Override
+    public List<Tile> getAllTiles() {
+        List<Tile> res = new ArrayList<>(super.getAllTiles());
+        if(firstPlayerTile != null) res.add(firstPlayerTile);
+        return res;
+    }
+
+    @Override
+    public List<Tile> removeAllTiles() {
+        List<Tile> res = new ArrayList<>(super.removeAllTiles());
+        if(firstPlayerTile != null){
+            res.add(firstPlayerTile);
+            firstPlayerTile = null;
+        }
+        return res;
     }
 }
 

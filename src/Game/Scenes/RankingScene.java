3c1ea4875df1351bbb6da27f5555bf33f5b3eb4d
VishalSurya2773 <100663505+VishalSurya2773@users.noreply.github.com>
2022-11-07 07:52:54 -0600

    Update RankingScene.java

--- src/Game/Scenes/RankingScene.java
+++ src/Game/Scenes/RankingScene.java
@@ -5,7 +5,23 @@ import Game.Backend.Game;
 
 // TODO
 public class RankingScene extends AbstractScene {
+    private List<GameObject> playerNames;
+    
+    private GameObject winText;
     public RankingScene(Game game){
-
+        super(game);
+        List<Player> players = game.getPlayers();
+        Collections.sort(players);
+        playerNames = new ArrayList<GameObject>();
+        for(Player p: players){
+            playerNames.add(p.getGameObject());   
+        }
+        
     }
+    public GameObject getWinner(){
+        return playerNames.get(0);  
+    }
+    
+    
+    
 }

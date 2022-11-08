54b5f835ef4eafc3fc0952fb50f4559ff3b422d4
Niknet1 <nikballu542@gmail.com>
2022-11-08 08:04:02 -0600

    tilesPlaced returns all tiles counted while scoring

---
+++ src/Game/Backend/PlaceTileResult.java
@@ -0,0 +1,17 @@
+package Game.Backend;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class PlaceTileResult {
+    private List<Tile> tileList;
+    private int scoreAdded;
+
+    public PlaceTileResult(List<Tile> t, int s){
+        tileList = t;
+        scoreAdded = s;
+    }
+    public int getScoreAdded(){
+
+    }
+}
